c8a200eff4bdeb1c62c226072927b5983605b334
Robin Schweigert <robin.schweigert@gmx.de>
2018-11-27 09:40:19 +0100

    readded user data writer and demographic input

--- KnuckleInput/app/src/main/java/uni/vis/janle/knuckleinput/MainActivity.java
+++ KnuckleInput/app/src/main/java/uni/vis/janle/knuckleinput/MainActivity.java
@@ -1,6 +1,7 @@
 package uni.vis.janle.knuckleinput;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,7 +11,11 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,10 +31,22 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        final EditText userID = findViewById(R.id.userIDText);
+        final EditText userID = findViewById(R.id.id_input);
+        final EditText userAge = findViewById(R.id.age_input);
+        final RadioButton femaleInput = findViewById(R.id.female_input);
+        final RadioButton maleInput = findViewById(R.id.male_input);
         final Button startButton = findViewById(R.id.startButton);
 
-
+        final File storeDirectory = new File(getApplicationContext().getFilesDir().getAbsolutePath());
+        System.out.println("directory: " + storeDirectory.getAbsolutePath());
+        int currentId = 0;
+        for (int i = 1; i < 99999; i++) {
+            if(!new File(storeDirectory, Integer.toString(i) + "_userData.csv").exists()) {
+                currentId = i;
+                userID.setText(Integer.toString(i));
+                break;
+            }
+        }
 
         startButton.setOnClickListener(new View.OnClickListener() {
 
@@ -37,6 +54,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (!TextUtils.isEmpty(userID.getText())) {
                     Toast.makeText(getApplicationContext(), "gj!", Toast.LENGTH_SHORT).show();
+                    String userGender = femaleInput.isChecked() ? "female" : "male";
+                    String userData = userID.getText() + ";" + userAge.getText() + ";" + userGender;
+                    FileOutputStream outputStream;
+                    try {
+                        outputStream = openFileOutput(userID.getText() + "_userData.csv", Context.MODE_APPEND);
+                        outputStream.write(userData.getBytes());
+                        outputStream.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                     startTask(v);
                 }
             }
