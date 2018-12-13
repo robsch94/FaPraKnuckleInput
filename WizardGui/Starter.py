8fa35c72b3db3fd2c794aaf7aaec7fa644102ca1
Robin Schweigert <robin.schweigert@gmx.de>
2018-12-13 13:28:36 +0100

    finished wizard gui

--- WizardGui/Starter.py
+++ WizardGui/Starter.py
@@ -3,11 +3,13 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from MainWindow import Ui_mainWindow
 from PyQt5.QtCore import QThread, QObject, pyqtSignal
 import socket
+import pyqtgraph as pg
+import numpy as np
 
 
 class Starter(QObject):
 
-    PHONE_IP = "127.0.0.1"
+    PHONE_IP = "192.168.0.102"
     PHONE_PORT = 1234
     cap_sig = pyqtSignal(str)
 
@@ -20,22 +22,35 @@ class Starter(QObject):
         self.__init_values()
         self.MainWindow.show()
         sys.exit(self.app.exec_())
-        self.ui.CapImage.set_image(None)
 
     def __received_data(self, data):
-        print(data)
+        try:
+            img = np.array(data.split(";")[-1].split(",")[2:-1], dtype=np.float64)
+            img[img < 0] = 0
+            img = img.reshape(27, 15).transpose()
+            self.img_view.setImage(img, autoLevels=False, levels=(0, 255))
+        except:
+            pass
 
     def __init_values(self):
+        self.img_view = pg.ImageView()
+        self.ui.img_widget.layout().addWidget(self.img_view)
+        self.img_view.ui.histogram.hide()
+        self.img_view.ui.roiBtn.hide()
+        self.img_view.ui.menuBtn.hide()
+
         self.com_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.udp_thread = UdpThread(self)
         self.udp_thread.start()
         self.cap_sig.connect(self.__received_data)
+        self.ui.next_btn.clicked.connect(self.__send_next)
+        self.ui.revert_btn.clicked.connect(self.__send_revert)
 
     def __send_next(self):
         self.com_sock.sendto("next".encode("UTF-8"), (Starter.PHONE_IP, Starter.PHONE_PORT))
 
     def __send_revert(self):
-        pass
+        self.com_sock.sendto("revert".encode("UTF-8"), (Starter.PHONE_IP, Starter.PHONE_PORT))
 
 class UdpThread(QThread):
 
@@ -44,12 +59,12 @@ class UdpThread(QThread):
         self.main = main
         PORT = 1234
         self.data_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.data_sock.bind(("127.0.0.1", Starter.PHONE_PORT))
+        self.data_sock.bind(("", Starter.PHONE_PORT))
         self.__running = True
 
     def run(self):
         while self.__running:
-            data, addr = self.data_sock.recvfrom(16384)
+            data, addr = self.data_sock.recvfrom(65536)
             self.main.cap_sig.emit(data.decode("UTF-8"))
 
     def stop(self):
--- WizardGui/capimageview.py
+++ WizardGui/capimageview.py
@@ -11,15 +11,12 @@ class  CapImageView(pg.GraphicsView):
         pg.GraphicsView.__init__(self, **kargs)
         self.setParent(parent)
         self.setWindowTitle('Cap Image View')
-        frame = np.random.normal(size=(100,100))
-        self.img_view = pg.ImageItem(frame)
-        self.addItem(self.img_view)
+        frame = np.random.normal(size=(100, 100))
+        self.img_view = pg.ImageView()
+        self.img_view.setImage(frame)
+        self.add(self.img_view)
 
     def set_image(self, im):
         self.img_view.set_image(im)
         print("received new image")
 
-if __name__ == '__main__':
-    w = ImageView()
-    w.show()
-    QtGui.QApplication.instance().exec_()
