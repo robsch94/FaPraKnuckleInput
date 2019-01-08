cc16558a3fec12f5814bc656ee0e8dad125baee7
J0ekr <jan.leusmann@gmx.de>
2019-01-08 10:10:29 +0100

    final tweeks interimpres

--- CapacitiveImageViewer/app/src/main/java/io/interactionlab/capimgdemo/ConturDetection.java
+++ CapacitiveImageViewer/app/src/main/java/io/interactionlab/capimgdemo/ConturDetection.java
@@ -1,31 +1,41 @@
+package io.interactionlab.capimgdemo;
+
 import org.hcilab.libftsp.capacitivematrix.blobdetection.BlobBoundingBox;
 import org.opencv.*;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
+import org.opencv.imgproc.Imgproc;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import static org.opencv.imgproc.Imgproc.CV_RETR_TREE;
+
 import static org.opencv.imgproc.Imgproc.THRESH_BINARY;
-import static org.opencv.imgproc.Imgproc.contourArea;
-import static org.opencv.imgproc.Imgproc.findContours;
 import static org.opencv.imgproc.Imgproc.threshold;
 
 public class ConturDetection {
 
-    public static int[][] getBlobContentIn27x15(int[][] matrix, BlobBoundingBox bbb) {
+    public static Float[] getBlobContent(int[][] matrix, BlobBoundingBox bbb) {
         Mat image = new Mat();
         Mat inv_image = new Mat();
         Core.bitwise_not(image, inv_image);
         double thresh = threshold(inv_image, image, 200, 255, THRESH_BINARY);
         List<MatOfPoint> contours = new ArrayList<>();
+        Mat hierarchy = new Mat();
         // anders als findContours von python??
-        findContours(image, contours, hierarchy, mode=CV_RETR_TREE=3,method=CV_CHAIN_APPROX_SIMPLE=2);
+        Imgproc.findContours(image, contours, hierarchy, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+
+        List<MatOfPoint> contours_extreme = new ArrayList<>();
+        Mat hierarchy2 = new Mat();
+        // anders als findContours von python??
+        Imgproc.findContours(image, contours_extreme, hierarchy2, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
 
         List<MatOfPoint> contours_filtered = new ArrayList<>();
+        /*
         Mat max_contour = new Mat();
+
         for (int i = 0; i < contours.size(); i++) {
             if (contourArea(contours.get(i)) > 5 && contourArea(contours.get(i)) > 255) {
                 contours_filtered.add(contours.get(i));
@@ -35,17 +45,34 @@ public class ConturDetection {
 
             }
         }
-
+        */
+        /*
         if (!contours_filtered.isEmpty()) {
             int[][] croped_im = new int[27][15];
             int xmin = 0;
             int xmax = 0;
             int ymin = 0;
             int ymax = 0;
+            for (int i = 0; i < max_contour.size(); i++) {
+
+            }
             // hier kommt ne haessliche for schleife hin
             return null;
         }
+        */
+        Float[] b = null;
+        if (!contours.isEmpty()) {
+            Integer[] a = Arrays.asList(contours_extreme.toArray()).toArray(new Integer[0]);
+            b = new Float[a.length];
+            for (int i = 0; i < a.length; i++) {
+                b[i] = a[i] / 255.0f;
+            }
+            return b;
+        }
         return null;
+
+    }
+
         /*
         crop_frame = 5
 
@@ -68,5 +95,5 @@ public class ConturDetection {
             else:
                 return (0, np.zeros((27,15)))
          */
-    }
+
 }
--- CapacitiveImageViewer/openCVLibrary341/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ CapacitiveImageViewer/openCVLibrary341/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\janle\\Downloads\\PalmTouch-master\\PalmTouch-master\\Android\\MobileHCI18-Demo\\CapacitiveImageViewer\\openCVLibrary341\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\janle\\Desktop\\GitHubDesktop\\FaPraKnuckleInput\\CapacitiveImageViewer\\openCVLibrary341\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
